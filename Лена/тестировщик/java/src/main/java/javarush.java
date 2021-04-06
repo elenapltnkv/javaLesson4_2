cb4c095ff5c2bcf9274bccf15c462e32b968b01d
Samelena <lyly-miranda@yandex.ru>
2021-04-06 14:05:25 +0300

    homeworkLesson4

--- Лена/тестировщик/java/src/main/java/javarush.java
+++ Лена/тестировщик/java/src/main/java/javarush.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class javarush {
-}
+public class javarush {
+            public static void main(String[] args) {
+            int x = 2;
+            int y = 12;
+
+             //y = x * y;
+             y = x + y;
+
+            x = y - x;
+            y = y - x;
+
+            System.out.println(x);
+            System.out.println(y);
+        }
+    }
+
